c8b2ccf5923b69326b5f5c82a2ea3a566688b678
FrenyJeffrin <freny1999@gmail.com>
2023-02-22 16:54:02 +0530

    collatz sequence

---
+++ Collatz/collatz.py
@@ -0,0 +1,12 @@
+﻿number=int(input())
+steps=0
+while number>1:
+    if number%2==0:
+        number=number//2
+        steps+=1
+        print(number)
+    else:
+        number=number*3+1
+        steps+=1
+        print(number)
+print("steps=",steps)    
